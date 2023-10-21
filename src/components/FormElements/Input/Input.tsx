7939302fa90ace863d2ce393936483dafbcf0e33
kaje94 <a.kajendran@gmail.com>
2023-10-21 11:36:11 +0530

    Pass input clsx for numeric input during ssr

--- src/components/FormElements/Input/Input.tsx
+++ src/components/FormElements/Input/Input.tsx
@@ -1,12 +1,18 @@
 "use client";
 import { clsx } from "clsx";
 import dynamic from "next/dynamic";
-import { ComponentProps, forwardRef } from "react";
+import { ComponentProps, createContext, forwardRef, useContext } from "react";
 import { Control } from "react-hook-form";
 import { FormFieldControllerProps } from "@/components/FormElements/Common";
 
+const NumericFormatLoadingContext = createContext<{ inputClassNames?: string }>({});
+
 const NumericFormat = dynamic(() => import("react-number-format").then((mod) => mod.NumericFormat), {
-    loading: () => <input className={clsx("input input-bordered w-full animate-pulse bg-transparent")} disabled={true} />,
+    loading: () => {
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        const ctx = useContext(NumericFormatLoadingContext);
+        return <input className={clsx("input input-bordered w-full animate-pulse bg-transparent", ctx?.inputClassNames)} disabled={true} />;
+    },
 });
 
 export interface ControllerProps extends FormFieldControllerProps, ComponentProps<"input"> {
@@ -23,17 +29,24 @@ export const Input = forwardRef<HTMLInputElement, Props>((props, ref) => {
 
     if (type === "number") {
         return (
-            <NumericFormat
-                className={clsx("input input-bordered w-full bg-transparent", error && "input-error", loading && "animate-pulse", inputClassNames)}
-                decimalScale={2}
-                decimalSeparator="."
-                disabled={loading}
-                getInputRef={ref}
-                thousandSeparator=","
-                {...rest}
-                defaultValue={rest.defaultValue as string | number}
-                value={rest.value as string | number}
-            />
+            <NumericFormatLoadingContext.Provider value={{ inputClassNames }}>
+                <NumericFormat
+                    className={clsx(
+                        "input input-bordered w-full bg-transparent",
+                        error && "input-error",
+                        loading && "animate-pulse",
+                        inputClassNames,
+                    )}
+                    decimalScale={2}
+                    decimalSeparator="."
+                    disabled={loading}
+                    getInputRef={ref}
+                    thousandSeparator=","
+                    {...rest}
+                    defaultValue={rest.defaultValue as string | number}
+                    value={rest.value as string | number}
+                />
+            </NumericFormatLoadingContext.Provider>
         );
     }
     return (
