3c05e1dfa33402259bd36116bf1eb43852068dea
kaje94 <a.kajendran@gmail.com>
2023-11-17 01:25:50 +0530

    Update margin top in autocomplete component

--- src/components/FormElements/AutoComplete/Autocomplete.tsx
+++ src/components/FormElements/AutoComplete/Autocomplete.tsx
@@ -86,7 +86,7 @@ export const Autocomplete = forwardRef<HTMLInputElement, Props>((props, ref) => 
 
                     {open && (
                         <div className={clsx("dropdown-end dropdown-open dropdown absolute inset-x-0 bottom-0 w-full")}>
-                            <div className={clsx("menu dropdown-content z-20 mt-1 grid w-full rounded rounded-t-none bg-base-200 p-0 shadow-lg")}>
+                            <div className={clsx("menu dropdown-content z-20 mt-0.5 grid w-full rounded rounded-t-none bg-base-200 p-0 shadow-lg")}>
                                 {filteredOptions.length === 0 && query !== "" ? (
                                     <div className="relative cursor-default select-none px-4 py-2 text-neutral">Nothing found.</div>
                                 ) : (
