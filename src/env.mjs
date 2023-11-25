6661b5b820971208e4b34b87a7933ad60259732f
kaje94 <a.kajendran@gmail.com>
2023-11-25 06:52:48 +0530

    Update depencencies

--- src/env.mjs
+++ src/env.mjs
@@ -33,19 +33,10 @@ export const env = createEnv({
      * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
      * middlewares) or client-side so we need to destruct manually.
      */
-    runtimeEnv: {
-        API_BASE_URL: process.env.API_BASE_URL,
-        AUTH0_SECRET: process.env.S3_UPLOAD_REGION,
-        AUTH0_BASE_URL: process.env.S3_UPLOAD_REGION,
-        AUTH0_ISSUER_BASE_URL: process.env.S3_UPLOAD_REGION,
-        AUTH0_CLIENT_ID: process.env.S3_UPLOAD_REGION,
-        AUTH0_CLIENT_SECRET: process.env.S3_UPLOAD_REGION,
-        NEXT_PUBLIC_IMAGE_CDN_BASE: process.env.NEXT_PUBLIC_IMAGE_CDN_BASE,
-        NEXT_PUBLIC_SUPPORT_EMAIL: process.env.NEXT_PUBLIC_SUPPORT_EMAIL,
-        NEXT_CONTACT_US_FORM_KEY: process.env.NEXT_CONTACT_US_FORM_KEY,
+    experimental__runtimeEnv: {
         NEXT_PUBLIC_RECAPTCHA_SITE_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY,
-        RECAPTCHA_SITE_SECRET: process.env.RECAPTCHA_SITE_SECRET,
-        VERCEL_URL: process.env.VERCEL_URL,
+        NEXT_PUBLIC_SUPPORT_EMAIL: process.env.NEXT_PUBLIC_SUPPORT_EMAIL,
+        NEXT_PUBLIC_IMAGE_CDN_BASE: process.env.NEXT_PUBLIC_IMAGE_CDN_BASE,
     },
     /**
      * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
