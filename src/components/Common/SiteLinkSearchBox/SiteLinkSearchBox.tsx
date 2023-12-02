3f71a5a480e0adad934cf49fe214f54840ca9dd0
kaje94 <a.kajendran@gmail.com>
2023-12-02 22:55:26 +0530

    Update search site link

--- src/components/Common/SiteLinkSearchBox/SiteLinkSearchBox.tsx
+++ src/components/Common/SiteLinkSearchBox/SiteLinkSearchBox.tsx
@@ -1,11 +1,12 @@
 import Head from "next/head";
+import Script from "next/script";
 import { FC } from "react";
 import { env } from "@/env.mjs";
 
 const structuredData = {
     "@context": "https://schema.org",
     "@type": "WebSite",
-    url: `${env.AUTH0_BASE_URL}/`,
+    url: env.AUTH0_BASE_URL,
     potentialAction: [
         {
             "@type": "SearchAction",
@@ -16,12 +17,10 @@ const structuredData = {
             "query-input": "required name=search_term_string",
         },
     ],
+    description:
+        "Effortlessly find your perfect vehicle on Targabay's marketplace. Explore diverse cars, bikes, and more with our intuitive search. Targabay simplifies the search for a seamless and personalized experience.",
 };
 
 export const SiteLinkSearchBox: FC = () => {
-    return (
-        <Head>
-            <script dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }} type="application/ld+json" />
-        </Head>
-    );
+    return <Script dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }} id="site-link-search" type="application/ld+json" />;
 };
