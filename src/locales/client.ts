fa400b7fc99f0ffc86fa282f2f9e47c019738f65
kaje94 <a.kajendran@gmail.com>
2023-12-17 17:06:58 +0530

    Revert "Update eslint json"
    This reverts commit c4c5f02549cdcb244838b3960d36de6cf197eb7a.

--- src/locales/client.ts
+++ src/locales/client.ts
@@ -1,4 +1,3 @@
-"use client";
 import { createI18nClient } from "next-international/client";
 
 export const { useI18n, useScopedI18n, I18nProviderClient, useChangeLocale, defineLocale, useCurrentLocale } = createI18nClient({
--- src/locales/server.ts
+++ src/locales/server.ts
@@ -1,4 +1,3 @@
-"use server";
 import { createI18nServer } from "next-international/server";
 
 export const { getI18n, getScopedI18n, getCurrentLocale, getStaticParams } = createI18nServer({
